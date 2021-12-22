d1747ce4767a5fa591d5baf7af7a01889e4bfaeb
Ninllo <yaozqiang2021@163.com>
2021-12-22 17:14:12 +0800

    hot-fix commit

--- src/main/java/com/ninllo/git/GitTest.java
+++ src/main/java/com/ninllo/git/GitTest.java
@@ -2,8 +2,7 @@ package com.ninllo.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello github");
-        System.out.println("hello git3");
+        System.out.println("hot-fix");
+
     }
 }
