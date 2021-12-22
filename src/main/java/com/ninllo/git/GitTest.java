805f92a560111c59363ede4321fb6380c3677799
Ninllo <yaozqiang2021@163.com>
2021-12-22 17:08:02 +0800

    second commit

--- src/main/java/com/ninllo/git/GitTest.java
+++ src/main/java/com/ninllo/git/GitTest.java
@@ -3,5 +3,6 @@ package com.ninllo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello github");
     }
 }
